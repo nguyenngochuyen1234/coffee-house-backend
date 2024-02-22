a2b407cb75a93bbad20ed90810e0347dea70e2a7
nguyenngochuyen1234 <103881376+nguyenngochuyen1234@users.noreply.github.com>
2024-02-22 22:35:28 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import 'dotenv/config.js'
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://app.netlify.com/sites/beautiful-pavlova-d4c397/deploys/65d76931176a6300783f8ecf",
   })
 );
 app.use(express.static('public'))
